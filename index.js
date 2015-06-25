3793979dd03f4e25efb75fc3fd9474a15ac2690e
Zach Pendleton <zachpendleton@gmail.com>
2015-06-25 00:42:15 -0600

    add player and play routes

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const io = require('socket.io')(http);
 const Game = require('./lib/models/game.js');
 const gameView = require('./lib/views/game.js');
 
+const Player = require('./lib/models/player.js');
+const playerView = require('./lib/views/player.js');
+
+const boardUtil = require('./lib/boardUtil.js');
+
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -50,9 +55,52 @@ app.post('/games', function(req, res) {
 });
 
 app.post('/games/:id/players', function(req, res) {
+  var game = DB[req.params.id];
+  var player = new Player(req.body.name);
+
+  if (game.isFull()) {
+    return res.send({ error: 'game full' });
+  }
+
+  game.players.push(player);
+  game.responses.push(res);
+
+  if (game.isFull()) {
+    game.start();
+    game.responses.shift().send(gameView(game));
+    game.startClock();
+  }
+
+  req.on('close', function() {
+    game.finish(game.currentPlayer);
+  });
 });
 
 app.post('/games/:id/plays', function(req, res) {
+  var game = DB[req.params.id];
+  var col = req.body.col;
+  var token = req.header('X-Token');
+
+  if (token !== game.currentPlayer.token) {
+    return res.status(400).send({ error: 'out of turn' });
+  }
+
+  if (!game.isActive()) {
+    return res.status(400).send({ error: 'game not active' });
+  }
+
+  game.responses.push(res);
+  if (!game.makePlay(col)) { return game.finish(game.otherPlayer()); }
+  if (boardUtil.isWon(game.currentPlayer.board)) { return game.finish(game.currentPlayer); }
+  if (boardUtil.isFull(game.board)) { return game.finish(); }
+
+  game.startClock();
+  game.currentPlayer = game.otherPlayer();
+  game.responses.shift().send(gameView(game));
+
+  req.on('close', function() {
+    game.finish(game.currentPlayer);
+  });
 });
 
 http.listen(3000, function() {
