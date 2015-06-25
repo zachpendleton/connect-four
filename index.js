9262aa7d72f5be83b980eea3b62a827afa2a15be
Zach Pendleton <zachpendleton@gmail.com>
2015-06-24 23:32:51 -0600

    add new game route

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * Connect Four as as Service (CFaaS)
+ *
+ */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Long = require('big-integer');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const Game = require('./lib/models/game.js');
+const gameView = require('./lib/views/game.js');
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// data storage
+const DB = {};
+
+// routes
+app.get('/games', function(req, res) {
+});
+
+app.get('/games/:id', function(req, res) {
+});
+
+app.post('/games', function(req, res) {
+  var game    = new Game();
+  DB[game.id] = game;
+
+  return res.send(gameView(game));
+});
+
+app.post('/games/:id/players', function(req, res) {
+});
+
+app.post('/games/:id/plays', function(req, res) {
+});
+
+http.listen(3000, function() {
+  console.log('listening on 3000');
+});
